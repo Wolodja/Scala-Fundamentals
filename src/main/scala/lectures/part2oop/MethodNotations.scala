2ef7f5ce6b432e790f265d0852386337e721bbb9
Włodzimierz Waśkowicz <wolodjawaskovych@i.ua>
2022-05-18 12:36:18 +0200

    Syntactic Sugar: Method Notations exercises

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -5,16 +5,26 @@ import scala.language.postfixOps
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
 
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
 
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
+
     def unary_! : String = s"$name, what the heck?!"
+
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
 
     def isAlive: Boolean = true
 
     def apply(): String = s"Hi, my name is $name, I like $favoriteMovie"
+
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times"
+
+    def learns(learns: String) = s"$name learns $learns"
+
+    def learnsScala: String = this learns "Scala"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -47,4 +57,12 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary()) // equivalent
+
+  println((mary + "the Rockstar") ())
+
+  println((+mary).age)
+
+  println(mary learns "Scala")
+  println(mary learnsScala)
+  println(mary(2))
 }
