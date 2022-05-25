a63bac3d7353acbf3ecee9e244a4b1219659f307
Włodzimierz Waśkowicz <wolodjawaskovych@i.ua>
2022-05-25 15:40:52 +0200

    Variance

--- src/main/scala/lectures/part2oop/Generics.scala
+++ src/main/scala/lectures/part2oop/Generics.scala
@@ -18,5 +18,22 @@ object Generics extends App {
 
   val emptyListOfIntegers = MyList.empty[Int]
 
-  
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  //List[Cat] extends List[Animal] ?
+  // 1. yes = COVARIANCE
+  class CovariantList[+A]
+  val animal: Animal = new Cat
+  val animalList : CovariantList[Animal] = new CovariantList[Cat]
+  //animalList.add(new Dog) ??? HARD QUESTION
+
+  // 2. NO = INVARIANCE
+  class InvariantList[A]
+  val invariantAnimalList: InvariantList[Animal] = new InvariantList[Animal]
+
+  // 3. Hell, no! CONTRAVARIANCE
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
 }
