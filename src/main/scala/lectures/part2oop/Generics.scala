26b925dbee1e7a723de37c9d4a8fb0f10f59f70f
Włodzimierz Waśkowicz <wolodjawaskovych@i.ua>
2022-05-27 15:31:13 +0200

    Generics: bounded types

--- src/main/scala/lectures/part2oop/Generics.scala
+++ src/main/scala/lectures/part2oop/Generics.scala
@@ -2,8 +2,13 @@ package lectures.part2oop
 
 object Generics extends App {
 
-  class MyList[A] { // use the type A}
+  class MyList[+A] { // use the type A}
 
+    def add[B >: A](element: B): MyList[B] = ???
+    /*
+    A = cat
+    B = dog => animal
+    */
   }
 
   val listOfIntegers = new MyList[Int]
@@ -26,8 +31,8 @@ object Generics extends App {
   // 1. yes = COVARIANCE
   class CovariantList[+A]
   val animal: Animal = new Cat
-  val animalList : CovariantList[Animal] = new CovariantList[Cat]
-  //animalList.add(new Dog) ??? HARD QUESTION
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+  //animalList.add(new Dog) ??? HARD QUESTION => we return a list of Animals
 
   // 2. NO = INVARIANCE
   class InvariantList[A]
@@ -36,4 +41,10 @@ object Generics extends App {
   // 3. Hell, no! CONTRAVARIANCE
   class Trainer[-A]
   val trainer: Trainer[Cat] = new Trainer[Animal]
+
+  //bounded types
+  class Cage[A <: Animal](animal: A) //A can only by a subtype of animal
+  val cage = new Cage(new Dog)
+
+
 }
