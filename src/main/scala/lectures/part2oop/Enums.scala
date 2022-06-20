d86cefb1e9d1a15b1974edd01770b724c53d1972
Włodzimierz Waśkowicz <wolodjawaskovych@i.ua>
2022-06-20 16:07:05 +0200

    Enums

---
+++ src/main/scala/lectures/part2oop/Enums.scala
@@ -0,0 +1,39 @@
+package lectures.part2oop
+
+object Enums {
+
+  enum Permissions {
+    case READ, WRITE, EXECUTE, NONE
+
+    // sff fields/methods
+    def openDocument() : Unit =
+      if(this == READ) println("opening document")
+      else println("reading not allowed.")
+  }
+
+  val somePermissions: Permissions = Permissions.READ
+
+  //constructor arguments
+  enum PermissionsWithBits(bits: Int) {
+    case READ extends PermissionsWithBits(4)  // 100
+    case WRITE extends PermissionsWithBits(2) // 010
+    case EXECUTE extends PermissionsWithBits(1) // 001
+    case NONE extends PermissionsWithBits(0) // 000
+  }
+
+  object PermissionsWithBits {
+    def fromBits(bits: Int): PermissionsWithBits =
+      PermissionsWithBits.NONE
+  }
+
+  // standard API
+  val somePermissionsOrdinal = somePermissions.ordinal
+  private val allPermissions = PermissionsWithBits.values // array of all possible of the enums
+  val readPEmission: Permissions = Permissions.valueOf("READ") // Permissions.READ
+
+  def main(args: Array[String]): Unit = {
+    somePermissions.openDocument()
+    println(somePermissionsOrdinal)
+    println(allPermissions.size)
+  }
+}
