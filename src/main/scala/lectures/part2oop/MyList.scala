b0003cbac241b29ef7198413eecb48ee9a1b5ce5
Włodzimierz Waśkowicz <wolodjawaskovych@i.ua>
2022-06-24 15:59:28 +0200

    Functions exercises

--- src/main/scala/lectures/part2oop/MyList.scala
+++ src/main/scala/lectures/part2oop/MyList.scala
@@ -14,9 +14,12 @@ abstract class MyList[+A] {
   def add[B >: A](element: B): MyList[B]
   def printElements: String
   override def toString: String = s"[${printElements}]"
-  def filter(predicate: MyPredicate[A]): MyList[A]
-  def map[B](transformer: MyTransformer[A, B]): MyList[B]
-  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+
+  // higher order functions
+  def filter(predicate: A => Boolean): MyList[A]
+  def map[B](transformer: A => B): MyList[B]
+  def flatMap[B](transformer: A => MyList[B]): MyList[B]
+
   def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
@@ -26,10 +29,12 @@ case object Empty extends MyList[Nothing] {
   def isEmpty: Boolean = true
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
-  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
-  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+
+  def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
+  def map[B](transformer: Nothing => B): MyList[B] = Empty
+  def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
+
   def ++[B >: Nothing](list: MyList[B]) : MyList[B] = list
-  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
 }
 
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -38,8 +43,9 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def isEmpty: Boolean = false
   def add[B >: A](element: B): MyList[B] = new Cons(element, this)
   def printElements: String = if (t.isEmpty) "" + h else s"$head ${tail.printElements}"
-  def filter(predicate: MyPredicate[A]): MyList[A] = {
-    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+
+  def filter(predicate: A => Boolean): MyList[A] = {
+    if (predicate(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
   }
   /*
@@ -49,8 +55,8 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
    = new Cons(2, new Cons(4, new Cons(6, Empty.map(n * 2))))
    = new Cons(2, new Cons(4, new Cons(6, Empty)))
   */
-  def map[B](transformer: MyTransformer[A, B]): MyList[B] = {
-    new Cons(transformer.transform(h), t.map(transformer))
+  def map[B](transformer: A => B): MyList[B] = {
+    new Cons(transformer(h), t.map(transformer))
   }
   /*
     [1,2] ++ [3,4,5]
@@ -67,17 +73,10 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     = [1,2] ++ [2,3] ++ Empty
     = [1,2,2,3]
   */
-  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
-    transformer.transform(h) ++ t.flatMap(transformer)
+  def flatMap[B](transformer: A => MyList[B]): MyList[B] =
+    transformer(h) ++ t.flatMap(transformer)
 }
 
-trait MyPredicate[-T] {
-  def test(element: T): Boolean
-}
-
-trait MyTransformer[-A, B] {
-  def transform(element: A): B
-}
 
 object ListTest extends App {
   val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
@@ -85,16 +84,16 @@ object ListTest extends App {
   println(listOfIntegers.toString)
   println(listOfStrings.toString)
   println(listOfIntegers.add("element").toString)
-  println(listOfIntegers.map(new MyTransformer[Int, Int] {
-    override def transform(element: Int): Int = element * 2
+  println(listOfIntegers.map(new Function1[Int, Int] {
+    override def apply(element: Int): Int = element * 2
   }).toString)
-  println(listOfIntegers.filter(new MyPredicate[Int] {
-    override def test(element: Int): Boolean = element % 2 == 0
+  println(listOfIntegers.filter(new Function1[Int, Boolean] {
+    override def apply(element: Int): Boolean = element % 2 == 0
   }).toString)
   val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, new Cons(6, Empty)))
   println((listOfIntegers ++ anotherListOfIntegers).toString)
-  println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]]{
-    override def transform(element: Int) = new Cons[Int](element, new Cons(element + 1, Empty))
+  println(listOfIntegers.flatMap(new Function1[Int, MyList[Int]]{
+    override def apply(element: Int) = new Cons[Int](element, new Cons(element + 1, Empty))
   }).toString)
   val cloneListOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
   println(listOfIntegers == cloneListOfIntegers)
--- src/main/scala/lectures/part3fp/WhatsAFunction.scala
+++ src/main/scala/lectures/part3fp/WhatsAFunction.scala
@@ -1,20 +1,17 @@
 package lectures.part3fp
 
 object WhatsAFunction extends App {
-
   // DREAM: use functions as first class elements
   // problem: oop
   val doubler = new MyFunction[Int, Int] {
     override def apply(element: Int): Int = element * 2
   }
   println(doubler(2))
-
   // function types = Function1[A,B]
   val stringToIntConverter = new Function1[String, Int] {
     override def apply(v1: String): Int = v1.toInt
   }
-  println(stringToIntConverter("3") + 4)
-
+  println(stringToIntConverter("3") + 5)
   val adder: ((Int, Int) => Int) = new Function2[Int, Int, Int] {
     override def apply(v1: Int, v2: Int): Int = v1 + v2
   }
@@ -23,6 +20,26 @@ object WhatsAFunction extends App {
 
   // ALL SCALA FUNCTIONS ARE OBJECTS
 
+  /*
+    1. a function which takes 2 strings and concatenates them
+    2. transform the MyPredicate and MyTransformer into function types
+    3. define a function which takes and int and returns another function which takes an int and returns an int
+      - what's the type of this function
+      - how to do it
+  */
+  val concat: (String, String) => String = new Function2[String, String, String] {
+    override def apply(v1: String, v2: String): String = v1 + v2
+  }
+
+  val supperAdder : Int => (Int => Int) = new Function1[Int, Function1[Int, Int]]{
+    override def apply(x: Int): Int => Int = new Function[Int, Int]{
+      override def apply(y: Int): Int = x + y
+    }
+  }
+
+  val adder3 = supperAdder(3)
+  println(adder3(4))
+  println(supperAdder(3)(4)) // curried function
 
 }
 
