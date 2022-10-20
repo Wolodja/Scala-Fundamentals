e33374655f6b3627b219f6d27d8d8f2a2ad0970a
Wolodja <wolodjawaskovych@i.ua>
2022-10-20 15:08:05 +0200

    Sequences and Ranges

---
+++ src/main/scala/lectures/part3fp/Sequences.scala
@@ -0,0 +1,18 @@
+package lectures.part3fp
+
+object Sequences extends App {
+
+  // Seq
+  val aSequence = Seq(1,3,2,4)
+  println(aSequence)
+  println(aSequence.reverse)
+  println(aSequence(2))
+  println(aSequence ++ Seq(5, 6, 7))
+  println(aSequence.sorted)
+
+  // Ranges
+  val aRange: Seq[Int] = 1 to 10
+  aRange.foreach(println)
+
+  (1 to 10).foreach(x => println("Hello"))
+}
