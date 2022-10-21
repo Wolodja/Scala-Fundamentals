5f5ea0595b4f1dcc877ae3f0eb63d6f7c0f59d8f
Wolodja <wolodjawaskovych@i.ua>
2022-10-21 12:12:26 +0200

    Vectors. Comparing list vs vector

--- src/main/scala/lectures/part3fp/Sequences.scala
+++ src/main/scala/lectures/part3fp/Sequences.scala
@@ -1,5 +1,7 @@
 package lectures.part3fp
 
+import scala.util.Random
+
 object Sequences extends App {
 
   // Seq
@@ -40,4 +42,34 @@ object Sequences extends App {
   // arrays and seq
   val numberSqe: Seq[Int] = numbers  // implicit conversion
   println(numberSqe)
+
+  // vectors
+  val vector: Vector[Int] = Vector(1,2,3)
+  println(vector)
+
+  // vectors vs lists
+
+  val maxRuns = 1000
+  val macCapacity = 1000000
+  def getWriteTime(collection: Seq[Int]): Double = {
+    val r = new Random
+    val times = for {
+      _ <- 1 to maxRuns
+    }   yield {
+      val currentTime = System.nanoTime()
+      collection.updated(r.nextInt(macCapacity), r.nextInt())
+      System.nanoTime() - currentTime
+    }
+    times.sum * 1.0 / maxRuns
+  }
+
+  val numbersList = (1 to macCapacity).toList
+  val numbersVector = (1 to macCapacity).toVector
+
+  // keep reference to tail
+  // updating an element in the middle takes long
+  println(getWriteTime(numbersList))
+  // depth of the tree is small
+  // needs to replace an etire 32-elements chunk
+  println(getWriteTime(numbersVector))
 }
