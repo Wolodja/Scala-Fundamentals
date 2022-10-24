368637f18a9add5cd1a6b4bb514f4d80ae559d1c
Wolodja <wolodjawaskovych@i.ua>
2022-10-24 16:57:41 +0200

    Maps exercises

--- src/main/scala/lectures/part3fp/TuplesAndMAps.scala
+++ src/main/scala/lectures/part3fp/TuplesAndMAps.scala
@@ -1,14 +1,16 @@
 package lectures.part3fp
 
+import scala.annotation.tailrec
+
 object TuplesAndMAps extends App {
 
   // tuples = finite ordered "lists"
-  val aTuple = new Tuple2(2, "Hello, Scala")  // Tuple2[Int, String] = (Int, String)
+  val aTuple = new Tuple2(2, "Hello, Scala") // Tuple2[Int, String] = (Int, String)
   val aSameTuple = (2, "Hello, Scala")
 
   println(aTuple._1)
   println(aTuple.copy(_2 = "goodbye Java"))
-  println(aTuple.swap)  // ("Hello, Scala", 2)
+  println(aTuple.swap) // ("Hello, Scala", 2)
 
   // Maps - keys -> values
   val aMap: Map[String, Int] = Map()
@@ -41,4 +43,85 @@ object TuplesAndMAps extends App {
   println(List(("Daniel" -> 555)).toMap)
   val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
   println(names.groupBy(name => name.charAt(0)))
+
+  val someMap = Map("Jim" -> 500, "JIM" -> 999)
+  println(someMap.map(pair => pair._1.toUpperCase -> pair._2))
+
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    network + (person -> Set())
+  }
+
+
+  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+  }
+
+  def unFriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA - b)) + (b -> (friendsB - a))
+  }
+
+
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+      if(friends.isEmpty) networkAcc
+      else removeAux(friends.tail, unFriend(networkAcc, friends.head, person))
+    }
+
+    removeAux(network(person), network) - person
+  }
+
+  def friendsNumber(network: Map[String, Set[String]], person: String): Int = {
+    network(person).size
+  }
+
+  def personWithMostFriends(network: Map[String, Set[String]]): String = {
+    network.maxBy(pair => pair._2.size)._1
+  }
+
+  def countOfPeopleWithNoFriends(network: Map[String, Set[String]]): Int = {
+    network.view.filterKeys(key => network(key).isEmpty).size
+  }
+
+  def countOfPeopleWithNoFriendsBetter(network: Map[String, Set[String]]): Int = {
+    network.count(_._2.isEmpty)
+  }
+
+  def socialConnection(network: Map[String, Set[String]], a: String, b: String): Boolean = {
+    @tailrec
+    def bfs(target: String, consideredPeople: Set[String], discoveredPeople: Set[String]): Boolean = {
+        if(discoveredPeople.isEmpty) false
+        else {
+          val person = discoveredPeople.head
+          if(person == target) true
+          else if (consideredPeople.contains(person)) bfs(target, consideredPeople, discoveredPeople.tail)
+          else bfs(target, consideredPeople + person, discoveredPeople.tail ++ network(person))
+        }
+      }
+
+    bfs(b, Set(), network(a) + a)
+  }
+
+  //  val empty: Map[String, Set[String]] = Map()
+  val network = add(add(Map(), "Mary"), "Bob")
+  println(network)
+  println(friend(network, "Bob", "Mary"))
+  println(unFriend(friend(network, "Bob", "Mary"), "Bob", "Mary"))
+  println(remove(friend(network, "Bob", "Mary"), "Bob"))
+
+  val people = add(add(add(Map(), "Bob"), "Mary"), "Jim")
+  val testNet = friend(friend(people, "Bob", "Jim"), "Bob", "Mary")
+  println(testNet)
+
+  println(personWithMostFriends(testNet))
+  println(countOfPeopleWithNoFriends(testNet))
+  println(countOfPeopleWithNoFriendsBetter(testNet))
+
+  println(socialConnection(testNet, "Mary", "Jim"))
+  println(socialConnection(testNet, "Mary", "Bob"))
 }
