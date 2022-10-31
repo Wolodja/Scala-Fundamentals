b469a9773d3bc8ee13b1f6e2eb8e6ba03341cf25
Wolodja <wolodjawaskovych@i.ua>
2022-10-31 08:11:32 +0100

    Options exercises

--- src/main/scala/lectures/part3fp/Options.scala
+++ src/main/scala/lectures/part3fp/Options.scala
@@ -1,5 +1,7 @@
 package lectures.part3fp
 
+import java.util.Random
+
 object Options extends App {
 
   val myFirstOption: Option[Int] = Some(4)
@@ -32,5 +34,42 @@ object Options extends App {
 
   // for-comprehensions
 
+  val config: Map[String, String] = Map(
+    "host" -> "176.45.36.1",
+    "port" -> "80"
+  )
 
+  class Connection {
+    def connect = "Connect" // connect to some server
+  }
+  object Connection {
+    val random = new Random(System.nanoTime())
+    def apply(host: String, port: String): Option[Connection] = {
+      if(random.nextBoolean()) Some(new Connection)
+      else None
+    }
+  }
+
+  val host = config.get("host")
+  val port = config.get("port")
+  val connection = host.flatMap(h => port.flatMap(p => Connection(h, p)))
+  val connectionStatus = connection.map(_.connect)
+  println(connectionStatus)
+  connectionStatus.foreach(println)
+
+  // chained calls
+  config.get("host")
+    .flatMap(h => config.get("port")
+      .flatMap(p => Connection(h, p))
+      .map(_.connect))
+    .foreach(println)
+
+  // for-comprehensions
+  val forConnectionStatus = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+
+  forConnectionStatus.foreach(println)
 }
