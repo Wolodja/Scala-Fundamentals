4ffa30e5c7acd5b0ac9b6588f66c6218a0aec358
Wolodja <wolodjawaskovych@i.ua>
2022-10-31 12:40:39 +0100

    Handling Failures Exercise

--- src/main/scala/lectures/part3fp/HandlingFailure.scala
+++ src/main/scala/lectures/part3fp/HandlingFailure.scala
@@ -1,5 +1,6 @@
 package lectures.part3fp
 
+import java.util.Random
 import scala.util.{Failure, Success, Try}
 
 object HandlingFailure extends App {
@@ -41,4 +42,36 @@ object HandlingFailure extends App {
   println(aSuccess.filter(_ > 10))
 
   // for-comprehensions
+
+  val hostname = "localhost"
+  val port = "8080"
+
+  def renderHTML(page: String): Unit = println(page)
+
+  class Connection {
+    def get(url: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html>...</html>"
+      else throw new RuntimeException("Connection interrupted")
+    }
+  }
+
+  object HttpService {
+    val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection = {
+      if (random.nextBoolean()) new Connection
+      else throw new RuntimeException("Someone else took the port")
+    }
+  }
+
+  Try(HttpService.getConnection(hostname, port))
+    .map(_.get("/home"))
+    .foreach(renderHTML)
+
+  // for-comprehension
+  for {
+    connection <- Try(HttpService.getConnection(hostname, port))
+    html <- Try(connection.get("home"))
+  } renderHTML(html)
 }
