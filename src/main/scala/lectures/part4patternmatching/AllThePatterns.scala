702f76160287a6a8dcfe037fcc7163b4a683b8f4
Wolodja <wolodjawaskovych@i.ua>
2022-11-08 09:43:13 +0100

    All patterns 2

--- src/main/scala/lectures/part4patternmatching/AllThePatterns.scala
+++ src/main/scala/lectures/part4patternmatching/AllThePatterns.scala
@@ -48,4 +48,29 @@ object AllThePatterns {
     case 1::List(_) => // infix pattern
     case List(1,2,3) :+ 42 => // infix patterns
 
+
+  // 6 - type specifiers
+  val unknown: Any = 2
+  val unknownMatch = unknown match
+    case list: List[Int] => // explicit type specifier
+    case _ =>
+
+  // 7 - name binding
+  val nameBindingMatch = aList match
+    case nonEmptyList @ Cons(_, _) => // name binding => use the name later here
+    case Cons(1, rest @ Cons(2, _)) => // name binding inside nested patterns
+
+  // 8 - multi-patterns
+  val multiPattern = aList match
+    case Empty | Cons(0, _) => // compound pattern (multi pattern)
+
+  // 9 - if guards
+  val secondElementSpecial = aList match
+    case Cons(_, Cons(specialElement, _ )) if specialElement % 2 == 0 =>
+
+  // ALL.
+
+
 }
+
+
